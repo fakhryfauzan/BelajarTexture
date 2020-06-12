554f296c078ed1499888e6eaca3b064f8d21ee2b
fakhryfauzan <fakhrysip@gmail.com>
2020-06-12 21:39:22 +0700

    Initial Commit

---
+++ BelajarTexture/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BelajarTexture
+//
+//  Created by Fadhil Brata   on 12/06/20.
+//  Copyright © 2020 fakhry fauzan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .gray
+    }
+
+
+}
+
